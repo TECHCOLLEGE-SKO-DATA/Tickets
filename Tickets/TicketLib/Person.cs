2bce53954a98c40e63ac2552ebf22cb979a2bda8
Emil Hytting <emilmm21@gmail.com>
2025-02-05 11:06:56 +0100

    updateClasses #21
    Updated the classes so now "Staff" is "Employee".
    
    Updated so the "Firstname" is now written as "FirstName" and so on.

--- Tickets/TicketLib/Person.cs
+++ Tickets/TicketLib/Person.cs
@@ -4,11 +4,11 @@ public class Person
 {
     public int PersonId { get; set; } //(PK)
 
-    public string Firstname { get; set; }
+    public string FirstName { get; set; }
 
-    public string Middlename { get; set; }
+    public string MiddleName { get; set; }
 
-    public string Lastname { get; set; }
+    public string LastName { get; set; }
 
     public int Address { get; set; } //(FK)
 
