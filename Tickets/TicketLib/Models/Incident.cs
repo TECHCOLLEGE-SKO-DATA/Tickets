5d129701bad5cdcf44c9e8c6a22f0d42d3325751
Emil Hytting <emilmm21@gmail.com>
2025-02-12 13:10:31 +0100

    Add addressId and customerId #54
    AddressId and CustomerId has been added to the class and the DbTest Script.

--- Tickets/TicketLib/Models/Incident.cs
+++ Tickets/TicketLib/Models/Incident.cs
@@ -5,17 +5,16 @@ namespace TicketLib;
 public class Incident : BaseModel, IModel
 {
     public int IncidentId { get; set; } //(PK)
-
+    public int? AddressId { get; set; }
+    public int CustomerId { get; set; } // (FK)
     public byte Status { get; set; }
-
     public DateTime IssueDate { get; set; }
-
     public string IssueDescription { get; set; } = " ";
-
     public int CreatedBy { get; set; } //(FK Staff)
-
     public DateTime ResolutionDate { get; set; }
     public string ResolutionDescription { get; set; } = " ";
+
+
 
     public string Validate()
     {
